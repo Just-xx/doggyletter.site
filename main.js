bddfb6fe0a4445772b72c8f61b33f7254bb8a017
Just-xx <kubadreczz@gmail.com>
2023-07-03 01:44:45 +0200

    add confirmation msg

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const popupTrigggerBtn = document.querySelector('.btn-container button')
 const popupWrapper = document.querySelector('.popup-wrapper')
 const popupDimm = document.querySelector('.popup-dimm')
 const popupIcon = document.querySelector('.popup-title i')
-const popupSubmit = document.querySelector('.popup-submit')
+const popupSubmitEl = document.querySelector('.popup-submit')
+
+const popupInput = document.querySelector('.popup-input');
+
+const msgMailEl = document.querySelector('.mail');
+const submitMsgEl = document.querySelector('.submit-msg');
 
 popupTrigggerBtn.addEventListener('click', () => {
   popupWrapper.classList.add('active');
@@ -19,8 +24,22 @@ const closePopup = () => {
   }, 150);
 }
 
+const submitPopup = () => {
+  submitMsgEl.classList.add('active-msg');
+  msgMailEl.textContent = popupInput.value;
+
+  setTimeout(() => submitMsgEl.style.transform = 'translateY(0)', 1);
+
+  setTimeout(() => submitMsgEl.style.transform = 'translateY(-100%)', 8000)
+  setTimeout(() => submitMsgEl.classList.remove('active-msg'), 8100)
+}
+ 
 popupDimm.addEventListener('click', closePopup)
 popupIcon.addEventListener('click', closePopup)
-popupSubmit.addEventListener('click', closePopup)
+
+popupSubmitEl.addEventListener('click', () => {
+  submitPopup();
+  closePopup();
+})
 
 popupWrapper.style.opacity = 0;
